71f324beaaca0a7381a8e7d1e610df9863b52378
Sid Pai <siddhantmpai@gmail.com>
2025-12-06 15:27:34 +0530

    fixed team-photos, slight  change to teamCard

--- parsec-frontend/src/pages/Schedule.jsx
+++ parsec-frontend/src/pages/Schedule.jsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import ComingSoon from '../components/ComingSoon.jsx';
-import Particles from '../components/Particles';
+import ComingSoon from "../components/ComingSoon.jsx";
+import Particles from "../components/Particles";
 import { useTheme } from "../context/ThemeContext";
+import ElectricBorder from "../components/ElectricBorder";
 
 function Schedule() {
   const { theme } = useTheme();
-  return <div>
-    <Navbar />
-    <Particles
-          particleColors={[theme.fontMain || "#ffffff", theme.fontMain || "#ffffff"]}
-          particleCount={400}
-          particleSpread={15}
-          speed={0.1}
-          particleBaseSize={70}
-          moveParticlesOnHover={false}
-          alphaParticles={false}
-          disableRotation={false}
-        />
-    <ComingSoon />
-    <Footer />
-  </div>;
+  return (
+    <div>
+      <Navbar />
+      <Particles
+        particleColors={[
+          theme.fontMain || "#ffffff",
+          theme.fontMain || "#ffffff",
+        ]}
+        particleCount={400}
+        particleSpread={15}
+        speed={0.1}
+        particleBaseSize={70}
+        moveParticlesOnHover={false}
+        alphaParticles={false}
+        disableRotation={false}
+      />
+
+      {/* <ElectricBorder
+        color="#7df9ff"
+        speed={1}
+        chaos={0.5}
+        thickness={2}
+        style={{ borderRadius: 16 }}
+      >
+        
+      </ElectricBorder> */}
+      <ComingSoon />
+      <Footer />
+    </div>
+  );
 }
 
 export default Schedule;
